c31708ca5ba0131396b71e0b0c484ca242d78c1f
Vijay Negi <vijaynegi217@gmail.com>
2026-06-27 20:44:10 +0530

    Fix Landing import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "@/pages/Landing";
+import Landing from "./pages/Landing";
 
 function App() {
     return (
