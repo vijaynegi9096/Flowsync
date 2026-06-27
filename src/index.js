ba6390dafd0e0c157baddc3a5c0eee9abfbb9665
Vijay Negi <vijaynegi217@gmail.com>
2026-06-27 20:24:13 +0530

    Fix App import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
-import App from "@/App";
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
